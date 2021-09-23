3cd0ceb6bf3cd2ae9b2cd2b56a16246c30c02f68
gobliggg <developer.iman@gmail.com>
2021-09-23 18:58:08 +0700

    Added health check endpoint

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
+const express = require('express')
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const app = express();
 
-const io = socketIO(server);
+// Add GET /health-check express route
+app.get('/health-check', (req, res) => {
+  res.status(200).send('OK');
+});
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+httpServer.listen(PORT);
+
